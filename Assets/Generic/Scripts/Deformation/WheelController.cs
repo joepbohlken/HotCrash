1ecb102cace954fd79d264eaa3bdf9162c17fd18
Pascal <440438@student.fontys.nl>
2022-09-30 12:07:36 +0200

    Car movement testing (v2) (discontinued)
    - Will no longer be worked on

--- Assets/Generic/Scripts/Deformation/WheelController.cs
+++ Assets/Generic/Scripts/Deformation/WheelController.cs
@@ -1,3 +1,5 @@
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEditor;
 using UnityEngine;
 
@@ -12,27 +14,89 @@ public class WheelController : MonoBehaviour
     [SerializeField] private float strength = 100f;
     [SerializeField] private float damping = 10f;
 
+    [Header("Raycast Properties")]
+    [Range(1, 30)]
+    [Tooltip("Number of rays being fired from the center of the wheel towards the ground. Set uneven number for one of those rays to be straight down.")]
+    [SerializeField] private int rayCount = 9;
+    [Range(0, 180)]
+    [Tooltip("The angle on which the rays will be spread across.")]
+    [SerializeField] private int discAngle = 135;
+
     private Rigidbody mainRb;
     private float force;
+    private NativeArray<RaycastCommand> raycastCommands;
+    private NativeArray<RaycastHit> raycastHits;
+
+    private Vector3 tempPos;
+
+    private void OnDestroy()
+    {
+        try { raycastCommands.Dispose(); raycastHits.Dispose(); } catch { }
+    }
+
+    private void OnDisable()
+    {
+        try { raycastCommands.Dispose(); raycastHits.Dispose(); } catch { }
+    }
 
     private void Start()
     {
         mainRb = GetComponentInParent<Rigidbody>();
+
+        // Initialize raycast commands
+        if (rayCount > 0)
+        {
+            raycastCommands = new NativeArray<RaycastCommand>(rayCount, Allocator.Persistent);
+            raycastHits = new NativeArray<RaycastHit>(rayCount, Allocator.Persistent);
+
+            float startAngle = rayCount == 1 ? 0 : -discAngle / 2f;
+            float stepAngle = rayCount == 1 ? 0 : (float)discAngle / (float)(rayCount - 1);
+            for (int i = 0; i < rayCount; i++)
+            {
+                //Debug.DrawRay(transform.position, Quaternion.AngleAxis(startAngle + stepAngle * i, -transform.right) * -transform.up * radius, Color.blue);
+                raycastCommands[i] = new RaycastCommand(transform.position, Quaternion.AngleAxis(startAngle + stepAngle * i, -transform.right) * -transform.up, radius);
+                Debug.Log(raycastCommands[i].direction);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
+        for (int i = 0; i < rayCount; i++)
+        {
+            RaycastCommand tempCom = raycastCommands[i];
+            tempCom.from = transform.position;
+            raycastCommands[i] = tempCom;
+        }
+
+        // Cast rays
+        JobHandle handle = RaycastCommand.ScheduleBatch(raycastCommands, raycastHits, 10);
+        handle.Complete();
+
+        float shortestDist = 999f;
+        RaycastHit? shortestHit = null;
+        for (int i = 0; i < rayCount; i++)
+        {
+            //Debug.DrawLine(raycastCommands[i].from, raycastHits[i].point, Color.black);
+
+            if (raycastHits[i].distance < shortestDist)
+            {
+                shortestDist = raycastHits[i].distance;
+                shortestHit = raycastHits[i];
+            }
+        }
+        tempPos = shortestHit.Value.point;
+
         float offset = 0f;
 
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, -transform.up, out hit, radius))
+        if (shortestHit != null)
         {
             // Calculate suspension
-            offset = radius - hit.distance;
+            offset = radius - shortestDist;
             float velocity = Vector3.Dot(transform.up, mainRb.GetPointVelocity(transform.position));
             force = (offset * strength) - (velocity * damping);
 
-            mainRb.AddForceAtPosition(transform.up * force, transform.position);
+            mainRb.AddForceAtPosition(shortestHit.Value.normal * force, transform.position);
         }
 
         // Update wheel object position
@@ -50,6 +114,14 @@ public class WheelController : MonoBehaviour
 
         Handles.color = Color.red;
         Handles.DrawLine(transform.position, transform.position + transform.up * force / 100f, 5f);
+
+        Handles.color = Color.black;
+        for (int i = 0; i < raycastHits.Length; i++)
+        {
+            Handles.DrawSolidDisc(raycastHits[i].point, transform.right, 0.03f);
+        }
+        Handles.color = Color.yellow;
+        Handles.DrawSolidDisc(tempPos, transform.right, 0.04f);
     }
 #endif
 }
