d6102c2ea3a1a84a76155b1f998f7fd6c4563e9c
Bohlken,Joep J <joepbohlken@gmail.com>
2022-10-04 14:55:58 +0200

    sound changes based on selected gear and rpm

--- Assets/Generic/Scripts/CarSound.cs
+++ Assets/Generic/Scripts/CarSound.cs
@@ -1,12 +1,17 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using static CarSound;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class CarSound : MonoBehaviour
 {
     [Serializable]
-    protected class Gears 
+    public class Gear
     {
         public string name;
         public AudioClip audioClip;
@@ -16,21 +21,60 @@ public class CarSound : MonoBehaviour
         public float maxAudioPitch;
     }
 
-    [SerializeField] private List<Gears> gears = new List<Gears>();
+    public List<Gear> gears = new List<Gear>();
 
-    [Range(0.75f, 1.5f)]
-    public float audioPitch = 0.75f;
-    public AudioSource audioSource;
+    // Debugging
+    [HideInInspector] public int gearIndex;
+    [Header("Debugging")]
+    [Range(0f, 1f)]
+    [SerializeField] private float _rpm;
+
+    private AudioSource audioSource;
+    private Gear currentGear;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        audioSource.pitch = audioPitch;
+        SetEngineSound(gearIndex, _rpm);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        audioSource.pitch = audioPitch;
+        SetEngineSound(gearIndex, _rpm);
+    }
+
+    public void SetEngineSound(int gearNumber, float rpm)
+    {
+        if (gears.IndexOf(currentGear) != gearNumber)
+        {
+            currentGear = gears.FirstOrDefault(g => g.name == "Gear" + (gearNumber + 1));
+            audioSource.clip = currentGear.audioClip;
+            audioSource.Play();
+        }
+
+        audioSource.pitch = Mathf.Lerp(currentGear.minAudioPitch, currentGear.maxAudioPitch, rpm);
     }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(CarSound))]
+public class GearDropdown : Editor
+{
+    private List<string> gearList = new List<string>();
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var myScript = target as CarSound;
+
+        gearList.Clear();
+        foreach(Gear gear in myScript.gears)
+        {
+            gearList.Add(gear.name);
+        }
+
+        myScript.gearIndex = EditorGUILayout.Popup("Current Gear", myScript.gearIndex, gearList.ToArray());
+    }
+}
+#endif
