d5a7690b4360743eaa8c9be43321a0df6059de1d
Joris Kamminga <joris.kamminga@gmail.com>
2022-10-29 02:42:57 +0200

    Added car selection screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public bool showStartAnim = true;
+    public Animator canvasAnimator;
+
     private Animator animator;
     private ParticleSystem particleSystem;
 
+    private bool multiplayerMenuOpen = false;
+    private int playerCount = 1;
 
     // Start is called before the first frame update
     private void Awake()
@@ -17,7 +22,17 @@ public class MainMenu : MonoBehaviour
 
     private void Start()
     {
-        animator.Play("Start");
+        if (showStartAnim)
+        {
+            animator.Play("MainMenuStart");
+        }
+        else
+        {
+            float time = .8f;
+            particleSystem.Simulate(time, true, true);
+            particleSystem.Pause();
+            particleSystem.time = time;
+        }
     }
 
     // Update is called once per frame
@@ -33,6 +48,41 @@ public class MainMenu : MonoBehaviour
 
     public void PauseParticleSystem()
     {
-        particleSystem.Pause();
+        particleSystem.Pause(true);
+        Debug.Log(particleSystem.time);
+    }
+
+    public void StartGame(int playerCount)
+    {
+        this.playerCount = playerCount;
+
+        ShowCarSelectionScreen(playerCount);
+    }
+
+    public void ShowMultiplayerSelection()
+    {
+        if (!multiplayerMenuOpen)
+        {
+            multiplayerMenuOpen = true;
+            canvasAnimator.Play("ShowMultiplayerSelect");
+        }
+    }
+    public void HideMultiplayerSelection()
+    {
+        if (multiplayerMenuOpen)
+        {
+            multiplayerMenuOpen = false;
+            canvasAnimator.Play("HideMultiplayerSelect");
+        }
+    }
+
+    public void ShowSettings()
+    {
+        HideMultiplayerSelection();
+    }
+
+    private void ShowCarSelectionScreen(int players = 1)
+    {
+        animator.Play("ShowCarSelection");
     }
 }
