e644131d29a698d39151ede3ca373b8d546b98af
Pascal <440438@student.fontys.nl>
2022-11-01 00:03:41 +0100

    Added explosion effect

--- Assets/Scripts/Abilities/MineObject.cs
+++ Assets/Scripts/Abilities/MineObject.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class MineObject : MonoBehaviour
 {
     [SerializeField] private Renderer highlight;
+    [SerializeField] private GameObject explosionEffectPrefab;
 
     [Space(12)]
     [SerializeField] private float explosionRadius = 10;
@@ -11,6 +13,7 @@ public class MineObject : MonoBehaviour
     private Transform owner;
     private Transform cars;
     private bool isSetUp = false;
+    private bool isTriggered = false;
 
     public void SetUpMine(Transform owner, Transform cars, bool isBot, Color highlightColor, int playerIndex)
     {
@@ -27,7 +30,8 @@ public class MineObject : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!isSetUp || other.transform.parent == owner) return;
+        if (!isSetUp || other.transform.parent == owner || isTriggered) return;
+        isTriggered = true;
 
         for (int i = 0; i < cars.childCount; i++)
         {
@@ -40,6 +44,15 @@ public class MineObject : MonoBehaviour
             }
         }
 
+        StartCoroutine(Explosion());
+    }
+
+    private IEnumerator Explosion()
+    {
+        GameObject explosion = Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(2f);
+        Destroy(explosion);
         Destroy(gameObject);
     }
 }
