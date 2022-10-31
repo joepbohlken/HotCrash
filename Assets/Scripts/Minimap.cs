9156c43cb3f84ea42931b63bfb8e1d16ba71eae8
Max Dussen <maxvdd76@gmail.com>
2022-10-31 15:53:12 +0100

    removed debugs from minimap script

--- Assets/Scripts/Minimap.cs
+++ Assets/Scripts/Minimap.cs
@@ -7,18 +7,12 @@ public class Minimap : MonoBehaviour
 {
     private Dictionary<Transform, GameObject> carsAndIndicators = new Dictionary<Transform, GameObject>();
 
-    //map props 166 - 245
-    //minimap props 80 - 123
-
     [SerializeField]
-    private GameObject carsParent;
+    private Color[] indicatorColors;
+    [SerializeField]
+    private GameObject carIndicatorPrefab;
 
-    public Color[] indicatorColors;
-    public GameObject carIndicatorPrefab;
-
-    void Start()
-    {
-    }
+    public GameObject carsParent;
 
     void Update()
     {
@@ -36,11 +30,10 @@ public class Minimap : MonoBehaviour
                 carsAndIndicators.Add(tf, indicator);
             }
         }
-
-        updatePostitions();
+        updateIndicatorPostitions();
     }
 
-    private void updatePostitions()
+    private void updateIndicatorPostitions()
     {
         foreach (KeyValuePair<Transform, GameObject> pair in carsAndIndicators)
         {
@@ -51,7 +44,6 @@ public class Minimap : MonoBehaviour
             /* X */    Mathf.Lerp(-86, 86, Mathf.Sign(tf.position.x / 166) == 1 ? Mathf.Lerp(0.5f, 1, tf.position.x / 166) : Mathf.Lerp(0.5f, 0f, Mathf.Abs(tf.position.x / 166))),
             /* Y */    Mathf.Lerp(-130, 130, Mathf.Sign(tf.position.z / 245) == 1 ? Mathf.Lerp(0.5f, 1, tf.position.z / 245) : Mathf.Lerp(0.5f, 0f, Mathf.Abs(tf.position.z / 245))));
 
-            if(tf.name.Contains("Player")) Debug.Log(tf.rotation.y);
             rt.localRotation = Quaternion.Euler(0, 0, -tf.rotation.eulerAngles.y + 90);
         }
     }
